e35eac1cfd2083539dbf5b5f6112130ec038e241
George Shaw <george.e.shaw.iv@outlook.com>
2017-12-10 19:50:12 -0600

    homepage done

--- application/document_root/js/landing.js
+++ application/document_root/js/landing.js
@@ -1,25 +1,31 @@
 jQuery(document).ready(function(){
-  var landing = jQuery('.beta-landing');
-  var content = jQuery('.beta-content');
+    var auth = 0;
+    var landing = jQuery('.beta-landing');
+    var content = jQuery('.beta-homepage-jumbotron');
 
-  var delay = 500;
+    if(auth == 0) {
+        var delay = 500;
 
-  landing.find('span').each(function(i){
-    var _this = this;
+        landing.find('span').each(function (i) {
+          var _this = this;
 
-    setTimeout(function(){
-      jQuery(_this).animate({
-        opacity: 1
-      }, 500);
-    }, delay);
+          setTimeout(function () {
+              jQuery(_this).animate({
+                  opacity: 1
+              }, 500);
+          }, delay);
 
-    delay += 500;
-  });
+          delay += 500;
+        });
 
-  setTimeout(function(){
-    landing.fadeOut("slow", function(){
-      landing.attr("style", "display:none !important");
-      content.fadeIn("slow");
-    });
-  }, 4000)
+        setTimeout(function () {
+          landing.fadeOut("slow", function () {
+              landing.attr("style", "display:none !important");
+              content.fadeIn("slow");
+          });
+        }, 4000)
+    } else {
+        landing.attr("style", "display:none !important");
+        content.attr("style", "display:block !important");
+    }
 });
